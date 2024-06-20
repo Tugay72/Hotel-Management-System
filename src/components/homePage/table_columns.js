8c6bb0940514d8fa4094f466d006ac3bf9dc4a59
Tugay <tasimtugay@gmail.com>
2024-06-20 21:05:20 +0300

    calculating prices by filtered data

--- src/components/homePage/table_columns.js
+++ src/components/homePage/table_columns.js
@@ -1,4 +1,7 @@
 import {Tag} from 'antd';
+import prices from '../room_prices'
+import { useState } from 'react';
+
 
 const allColumns = [
     {
@@ -64,14 +67,72 @@ const allColumns = [
     },
     {
       title: 'Price',
-      dataIndex: 'price',
+      dataIndex: "price",
       key: 'price',
       align : 'center',
       width : 196,
       defaultSortOrder: '',
       sorter: (a, b) => a.price - b.price,
-      render: (text) => `$${text}`,
+      render: (text, record) => (
+        <span>
+          <p style={{fontSize : '0.75rem', color : 'black'}}>{calculatePrice(record.room_type, '2024-07-01', '2024-07-04')}</p>
+        </span>
+      ),
+      //render: (text) => `$${text}`,
     },
-  ];
+];
+
+const calculatePrice = (roomType, startDate, endDate) => {
+  const room = prices.find(room => room.key === roomType);
+  let totalPrice = 0;
+  const days = (new Date(endDate) - new Date(startDate)) / (1000 * 60 * 60 * 24);
+
+  if (room) {
+    if (room.priceByDate.length !== 0) {
+      var calculatedDays = days;
+      console.log(days, calculatedDays)
+      while (calculatedDays > 0) {
+
+
+        room.priceByDate.some(data => {
+          const dataStartDate = new Date(data.startDate);
+          const dataEndDate = new Date(data.endDate);
+          if (new Date(startDate) <= dataStartDate && new Date(endDate) >= dataStartDate && new Date(endDate) <= dataEndDate) {
+            totalPrice += data.price * (1 + ((new Date(endDate) - dataStartDate) / (1000 * 60 * 60 * 24)));
+            calculatedDays -= ((new Date(endDate) - dataStartDate) / (1000 * 60 * 60 * 24)) + 1;
+          } 
+          else if (new Date(startDate) >= dataStartDate && new Date(endDate) <= dataEndDate) {
+            totalPrice += data.price * days;
+            calculatedDays = 0;
+          } 
+          else if (new Date(startDate) <= dataEndDate && new Date(endDate) >= dataEndDate) {
+            totalPrice += data.price * +((dataEndDate - new Date(startDate)) / (1000 * 60 * 60 * 24));
+            calculatedDays -= ((dataEndDate - new Date(startDate)) / (1000 * 60 * 60 * 24));
+          } 
+          else if (new Date(startDate) <= dataStartDate && new Date(endDate) >= dataEndDate) {
+            totalPrice += data.price * ((dataEndDate - dataStartDate) / (1000 * 60 * 60 * 24));
+            calculatedDays -= ((dataEndDate - dataStartDate) / (1000 * 60 * 60 * 24));
+          }
+          else{
+            totalPrice += calculatedDays * room.basePrice;
+            return;
+            
+          }
+        });
+        
+      }
+      if (calculatedDays > 0) {
+        totalPrice += calculatedDays * room.basePrice;
+      }
+    } 
+    else {
+      totalPrice = room.basePrice * days;
+    }
+  }
+  return totalPrice;
+};
+
+
+
 
 export default allColumns;
--- src/components/room_prices.js
+++ src/components/room_prices.js
@@ -1,25 +1,33 @@
 const prices = [
     {
     "key" : "Tek",
-    "basePrice" : 50,
+    "basePrice" : 100,
     "priceByDate" : [
-
-    ]
+        {
+            'startDate' : '2024-07-05',
+            'endDate' : '2024-07-12',
+            'price' : 10
+        },
+        {
+            'startDate' : '2024-07-13',
+            'endDate' : '2024-07-18',
+            'price' : 1
+        }
+    ],
     },
     {
     "key" : "Çift",
     "basePrice" : 75,
     "priceByDate" : [
         
-    ]
-
+    ],
     },
     {
     "key" : "Aile",
     "basePrice" : 100,
     "priceByDate" : [
         
-    ]
+    ],
     }
 ];
 
